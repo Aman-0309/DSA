2994c4a72f2b63e749f3d0298e9fe6c02f1f8a9a
Aman-0309 <amankumar78045@gmail.com>
2025-09-01 08:03:39 +0530

    single no problem

---
+++ C++/vector/singleNo.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main (){
+
+// // for each loop => 
+//   vector <int> nums = { 1, 3, 5, 7};
+
+//    cout << "The size of the vector is : " << nums.size() << endl;
+//      cout << "The elements are: " ;
+//   for(int value : nums){
+//     cout << val << " ";
+//   }
+
+
+
+
+// Single number problem (136);
+
+vector <int> nums = {1, 5, 1, 2, 2};
+int ans = 0;
+
+for (int value : nums){
+  ans = ans ^ value;
+}
+cout << "The single number is : " << ans;
+
+
+}
