00af646340b08d7ca8b7427593c2b1d945570f8a
Aman-0309 <amankumar78045@gmail.com>
2025-10-07 19:21:43 +0530

    Linear Search algo

---
+++ C++/Algorithms/LinearSearch.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+//<<<<<<<<<<<<<<<<<<<<< linear search >>>>>>>>>>>>>>>>>
+cout <<"Lets apply linear search in the array"<< endl;
+    int arr[100] , size , target;
+    cout << "Enter the size of an array : ";
+    cin >> size;
+    // input the array
+    cout << "Enter the array : ";
+    for(int i=0; i<size; i++){
+        cin >> arr[i];
+    }
+
+    cout << "Enter the target from the array : ";
+    cin >> target;
+  // linear search
+   bool found = false;      // flag which will be updated later 
+    for(int i=0; i<size; i++){
+       if(arr[i] == target){
+        cout <<"The target is at "<<  (i + 1) << endl;
+        found = true;       // flag found 
+       }
+
+       }
+        if(!found){
+        cout << "Target not found!" << endl;
+    }
+}
