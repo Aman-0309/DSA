da8021604dafe72c4f24d1ac9acddab926a35386
Aman-0309 <amankumar78045@gmail.com>
2025-10-14 01:00:36 +0530

    Palindrome check

---
+++ C++/Strings/string2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <cctype>  // for isalnum() and tolower()
+using namespace std;
+
+int main(){
+
+    // Palindrome check
+
+    string str = "A man, a plan, a canal: Panama";
+    bool isPalindrome = true; //flag
+
+    string str1 = "";
+
+    for(char ch : str){
+        if(isalnum(ch)){
+            str1 += tolower(ch);
+        }
+    }
+    cout << "String : " << str1 <<endl;
+    
+    int start = 0 , end = str1.size()-1;
+    while(start < end){
+        if(str1[start] != str1[end]){
+            return false;
+            break;
+        }
+        start++;
+        end--;
+    }
+    if(isPalindrome){
+        cout << "Result : Valid palindrome";
+    }else{
+        cout << "Result : Not a Valid palindrome";
+    }
+
+}
